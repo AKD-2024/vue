97accd454ed262f043fa193c879ace9dcf33197b
Anjali <anjali.dubey@comprotechnologies.com>
2024-08-08 17:53:55 +0530

    Vue.js : Training.

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "https://google.com",
       showBooks: true,
-      title: "Matilda",
-      author: "Roald Dahl",
-      review: "Recommended - Amazing book!!",
-      age: 40,
+      books: [
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+          img: "https://cdn.pixabay.com/photo/2024/08/03/11/31/ai-generated-8941975_1280.png",
+          isFav: true,
+        },
+        {
+          title: "the way of kings",
+          author: "brandon sanderson",
+          img: "https://cdn.pixabay.com/photo/2024/08/03/11/31/ai-generated-8941975_1280.png",
+          isFav: false,
+        },
+        {
+          title: "the final empire",
+          author: "brandon sanderson",
+          img: "https://cdn.pixabay.com/photo/2024/08/03/11/31/ai-generated-8941975_1280.png",
+          isFav: true,
+        },
+      ],
+      //   title: "Matilda",
+      //   author: "Roald Dahl",
+      //   review: "Recommended - Amazing book!!",
+      //   age: 40,
+      //   x: 0,
+      //   y: 0,
     };
   },
   methods: {
@@ -29,8 +52,9 @@ const app = Vue.createApp({
     },
 
     handleMousemove(e) {
-        
-    }
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
   },
 });
 
