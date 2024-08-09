157ba6bd63a109b52936f03a12e865ba77189ed5
Anjali <anjali.dubey@comprotechnologies.com>
2024-08-09 18:02:56 +0530

    Vue: Training.

--- app.js
+++ app.js
@@ -44,6 +44,10 @@ const app = Vue.createApp({
       this.showBooks = !this.showBooks;
     },
 
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+    },
+
     handleEvent(e, data) {
       console.log(e, e.type);
       if (data) {
@@ -59,3 +63,7 @@ const app = Vue.createApp({
 });
 
 app.mount("#app");
+
+// Challenge - Add to Favs
+// - attach a click event to each li tag (for each book)
+// - when a user clicks an li, toggle the 'isFav' property of that book
